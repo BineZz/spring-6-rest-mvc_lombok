5fb32064126290713855048f747eecd7c49ecc36
Sabino <binobapt@gmail.com>
2023-06-02 18:20:34 +0100

    Builder and Constructors

---
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.model.Beer;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public interface BeerService {
+
+    Beer getBeerById(UUID id);
+}
